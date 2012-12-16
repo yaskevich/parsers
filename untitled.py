f872d48ceb3999634efecce12c714b5787325aa1
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2012-12-16 12:34:08 -0800

    Create untitled.py

---
+++ untitled.py
@@ -0,0 +1,10 @@
+import sys
+
+import xml.etree.ElementTree as ET
+
+#with open ("data.txt", "r") as myfile:
+#    data=myfile.read().replace('\n', '')
+# sys.stdout.write(data)
+
+
+#tree = ET.fromstring(data)
