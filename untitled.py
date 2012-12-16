df4a3eb1785174de11933d633ee36251adf288fb
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2012-12-16 21:43:02 +0100

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,10 +1,20 @@
 import sys
 
 import xml.etree.ElementTree as ET
-
+header = '<?xml version="1.0" encoding="UTF-8"?>'
+# 1. Read the file
 #with open ("data.txt", "r") as myfile:
 #    data=myfile.read().replace('\n', '')
 # sys.stdout.write(data)
 
+# 2. Convert content utf-8 encoding
+
+# 3. Load content into XML-processor
 
 #tree = ET.fromstring(data)
+
+# 4. Parse content, get the data
+
+# 5. Save the data in separate file.
+
+# ... Profit!
