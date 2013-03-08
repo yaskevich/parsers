6f1b85d6adbaef627e335a4987b0f53b479549e0
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2013-03-08 22:55:43 +0100

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,20 +1,21 @@
 import sys
-
+import codecs
+import os.path
 import xml.etree.ElementTree as ET
-header = '<?xml version="1.0" encoding="UTF-8"?>'
-# 1. Read the file
-#with open ("data.txt", "r") as myfile:
-#    data=myfile.read().replace('\n', '')
-# sys.stdout.write(data)
 
-# 2. Convert content into utf-8 encoding
-
-# 3. Load content into XML-processor
-
-#tree = ET.fromstring(data)
-
-# 4. Parse content, get the data
-
-# 5. Save the data in separate file.
-
-# ... Profit!
+# checking 1st argument of teh scipt: it must be 
+if len(sys.argv) < 2: sys.exit('Usage: %s XML-file' % sys.argv[0])
+# checking whether it does exist
+if not os.path.exists(sys.argv[1]): sys.exit('ERROR: XML-file %s was not found!' % sys.argv[1])
+# reading the file into variable
+with open (sys.argv[1], "r") as myfile: data=myfile.read()
+# decoding - XML must be UTF-8
+body = data.decode("windows-1251")
+# appending a XML-header
+xmldoc = '<?xml version="1.0" encoding="UTF-8"?>' + body.encode("utf-8")
+# parsing xml-chunk
+root = ET.fromstring(xmldoc)
+# getting the item(s) we need
+item = root.findall("./bibliography/code")
+# iterate through items, getting the content
+for neighbor in item: print neighbor.text.strip()
